2b884cba6e4a686970a6f33a69ceec1b4ce6da5a
Martin Gantenbein <martin.gantenbein@gmail.com>
2015-07-19 10:02:22 +0000

    Add new role for external members

--- app/models/group/team.rb
+++ app/models/group/team.rb
@@ -1,7 +1,25 @@
 # encoding: utf-8
 
-class Group::Team < Group::Club
+class Group::Team < ::Group
 
   self.layer = false
 
+  self.event_types = []
+
+  children Group::Team
+
+  ### ROLES
+  class Administrator < Role::Administrator
+  end
+
+  class Leader < Role::Leader
+  end
+
+  class Member < Role::Member
+  end
+
+  class ExternalMember < Role::ExternalMember
+  end
+
+  roles Administrator, Leader, Member, ExternalMember
 end
---
+++ app/models/role/external_member.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+class Role::ExternalMember < Role::Member
+    self.visible_from_above = false;
+end
