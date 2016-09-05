942c5fe3e754b0d7bcc9fc00aaccd9aac9a8da16
MartinGantenbein <martin.gantenbein@gmail.com>
2016-09-05 22:05:46 +0200

    Downgrade permissions from layer_and_below to group_and_below

--- app/models/role/administrator.rb
+++ app/models/role/administrator.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 class Role::Administrator < ::Role
-  self.permissions = [:admin, :layer_and_below_full]
+  self.permissions = [:group_and_below_full]
 end
--- app/models/role/leader.rb
+++ app/models/role/leader.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 class Role::Leader < ::Role
-  self.permissions = [:layer_and_below_read]
+  self.permissions = [:group_and_below_read]
 end
