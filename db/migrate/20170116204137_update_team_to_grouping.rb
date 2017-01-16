7fdefbc5fe4f2ba07fe3db001fae5f6f0edf9266
MartinGantenbein <martin.gantenbein@gmail.com>
2017-01-17 00:02:58 +0100

    Fix SQL syntax

--- db/migrate/20170116204137_update_team_to_grouping.rb
+++ db/migrate/20170116204137_update_team_to_grouping.rb
@@ -1,10 +1,10 @@
 class UpdateTeamToGrouping < ActiveRecord::Migration
   def change
     execute "UPDATE groups SET type = 'Group::Grouping', layer_group_id = parent_id WHERE type = 'Group::Team' and parent_id <> 1;"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping;"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping;"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping;"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping;"
+    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping';"
+    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping';"
+    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping';"
+    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from groups parent where parent.id = g.parent_id) where g.type = 'Group::Grouping';"
   end
   
   def down
