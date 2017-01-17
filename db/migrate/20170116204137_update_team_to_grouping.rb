9ff8ffe4852cf1782ab4cbe6edfa55c41559f9c1
MartinGantenbein <martin.gantenbein@gmail.com>
2017-01-17 23:16:22 +0100

    Update roles as well

--- db/migrate/20170116204137_update_team_to_grouping.rb
+++ db/migrate/20170116204137_update_team_to_grouping.rb
@@ -1,7 +1,9 @@
 class UpdateTeamToGrouping < ActiveRecord::Migration
   def change
-    execute "UPDATE groups g SET g.type = 'Group::Grouping', layer_group_id = g.parent_id WHERE g.type = 'Group::Team' and g.id not in (19, 21);"
+    execute "UPDATE groups g SET g.type = 'Group::Grouping', layer_group_id = g.parent_id WHERE g.type = 'Group::Team' and g.id not in (select group_id from roles where type in ('Group::Team::Leader', 'Group::Team::Administrator'));"
     execute "UPDATE groups g join groups parent on (g.parent_id = parent.id and g.type = parent.type) set g.layer_group_id = parent.layer_group_id where g.type = 'Group::Grouping';"
+    execute "UPDATE roles set type = 'Group::Grouping::Member' where type = 'Group::Team::Member' and group_id in (select id from groups where type = 'Group::Grouping');"
+    execute "UPDATE roles set type = 'Group::Grouping::ExternalMember' where type = 'Group::Team::ExternalMember' and group_id in (select id from groups where type = 'Group::Grouping');"
   end
   
   def down
