cbfbfab501749fe0cdaf909ee6ec2bb3a2eaf5f0
MartinGantenbein <martin.gantenbein@gmail.com>
2017-01-17 22:15:35 +0100

    Correct UPDATE statement

--- db/migrate/20170116204137_update_team_to_grouping.rb
+++ db/migrate/20170116204137_update_team_to_grouping.rb
@@ -1,10 +1,7 @@
 class UpdateTeamToGrouping < ActiveRecord::Migration
   def change
-    execute "UPDATE groups SET type = 'Group::Grouping', layer_group_id = parent_id WHERE type = 'Group::Team' and parent_id <> 1;"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from (select layer_group_id from groups where id = parent_id) parent) where g.type = 'Group::Grouping';"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from (select layer_group_id from groups where id = parent_id) parent) where g.type = 'Group::Grouping';"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from (select layer_group_id from groups where id = parent_id) parent) where g.type = 'Group::Grouping';"
-    execute "UPDATE groups g SET g.layer_group_id = (select parent.layer_group_id from (select layer_group_id from groups where id = parent_id) parent) where g.type = 'Group::Grouping';"
+    execute "UPDATE groups g SET g.type = 'Group::Grouping', layer_group_id = g.parent_id WHERE g.type = 'Group::Team' and g.id not in (19, 21);"
+    execute "UPDATE groups g join groups parent on (g.parent_id = parent.id and g.type = parent.type) set g.layer_group_id = parent.layer_group_id where g.type = 'Group::Grouping';"
   end
   
   def down
